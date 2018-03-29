e7cb6400306b75ceda7336d6d4b754856aba3b74
Sergey Kravchenko <sergey@craftworks.ru>
2018-03-29 14:46:32 +0300

    refactored render function

--- src/GenDiff.php
+++ src/GenDiff.php
@@ -3,37 +3,35 @@
 namespace Craftworks\GenDiff;
 
 use Symfony\Component\Yaml\Yaml;
+use Illuminate\Support\Collection;
 
-function diff($format, $firstFile, $secondFile)
+function compare($firstFile, $secondFile, $format = 'pretty')
 {
     $file1 = file_get_contents($firstFile);
     $file2 = file_get_contents($secondFile);
-    $parser = __NAMESPACE__ . '\\' . pathinfo($firstFile, PATHINFO_EXTENSION) . 'Parse';
-    $data1 = $parser($file1);
-    $data2 = $parser($file2);
-    $tree = build($data1, $data2);
+    $parse = [
+        'json' => function ($json) {
+            return json_decode($json);
+        },
+        'yml' => function ($yaml) {
+            return Yaml::parse($yaml, Yaml::PARSE_OBJECT_FOR_MAP);
+        }
+    ];
+    $inputFormat = pathinfo($firstFile, PATHINFO_EXTENSION);
+    $data1 = $parse[$inputFormat]($file1);
+    $data2 = $parse[$inputFormat]($file2);
+    $tree = buildDiff($data1, $data2);
     return render($tree);
 }
 
-function jsonParse($json)
-{
-    return json_decode($json);
-}
-
-function ymlParse($yaml)
-{
-    return Yaml::parse($yaml, Yaml::PARSE_OBJECT_FOR_MAP);
-}
-
-function build($data1, $data2)
+function buildDiff($data1, $data2)
 {
     $arrData1 = boolToString(get_object_vars($data1));
     $arrData2 = boolToString(get_object_vars($data2));
     $keys = \Funct\Collection\union(array_keys($arrData1), array_keys($arrData2));
     $result = array_reduce($keys, function ($acc, $key) use ($arrData1, $arrData2) {
-        $acc[] = getNode($key, $arrData1, $arrData2);
-        return $acc;
-    }, []);
+        return $acc->push(getNode($key, $arrData1, $arrData2));
+    }, collect());
     return $result;
 }
 
@@ -53,35 +51,56 @@ function boolToString($array)
 function getNode($key, $data1, $data2)
 {
     if (!key_exists($key, $data1)) {
-        return ['type' => 'added', 'key' => $key, 'value' => $data2[$key]];
+        return [
+            'type' => 'added',
+            'key' => $key,
+            'afterValue' => $data2[$key]
+        ];
     }
     if (!key_exists($key, $data2)) {
-        return ['type' => 'removed', 'key' => $key, 'value' => $data1[$key]];
+        return [
+            'type' => 'removed',
+            'key' => $key,
+            'beforeValue' => $data1[$key]
+        ];
     }
     if ($data1[$key] === $data2[$key]) {
-        return ['type' => 'equal', 'key' => $key, 'value' => $data1[$key]];
+        return [
+            'type' => 'equal',
+            'key' => $key,
+            'beforeValue' => $data1[$key],
+            'afterValue' => $data2[$key]
+        ];
     }
-    return ['type' => 'changed', 'key' => $key, 'value' => $data2[$key], 'prevValue' => $data1[$key]];
+    return [
+        'type' => 'changed',
+        'key' => $key,
+        'beforeValue' => $data1[$key],
+        'afterValue' => $data2[$key]
+    ];
 }
 
 function render($tree)
 {
-    $result = array_reduce($tree, function ($acc, $node) {
-        return $acc . renderNode($node);
-    }, '');
-    return "{\n{$result}}";
+    $result = $tree
+        ->map(function ($node) {
+            return renderNode($node);
+        })
+        ->flatten()
+        ->all();
+    return "{\n" . implode("\n", $result) . "\n}\n";
 }
 
 function renderNode($node)
 {
     switch ($node['type']) {
         case 'added':
-            return "  + {$node['key']}: {$node['value']}\n";
+            return ["  + {$node['key']}: {$node['afterValue']}"];
         case 'removed':
-            return "  - {$node['key']}: {$node['value']}\n";
+            return ["  - {$node['key']}: {$node['beforeValue']}"];
         case 'equal':
-            return "    {$node['key']}: {$node['value']}\n";
+            return ["    {$node['key']}: {$node['beforeValue']}"];
     }
-    return "  + {$node['key']}: {$node['value']}\n" .
-           "  - {$node['key']}: {$node['prevValue']}\n";
+    return ["  + {$node['key']}: {$node['afterValue']}",
+           "  - {$node['key']}: {$node['beforeValue']}"];
 }
--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -8,18 +8,15 @@ class GenDiffTest extends \PHPUnit\Framework\TestCase
     {
         $expected1 = file_get_contents('tests/fixtures/expected1.txt');
         $expected2 = file_get_contents('tests/fixtures/expected2.txt');
-        $this->assertEquals(\Craftworks\GenDiff\diff(
-            'pretty',
+        $this->assertEquals(\Craftworks\GenDiff\compare(
             'tests/fixtures/before1.json',
             'tests/fixtures/after1.json'
         ), $expected1);
-        $this->assertEquals(\Craftworks\GenDiff\diff(
-            'pretty',
+        $this->assertEquals(\Craftworks\GenDiff\compare(
             'tests/fixtures/before2.json',
             'tests/fixtures/after2.json'
         ), $expected2);
-        $this->assertEquals(\Craftworks\GenDiff\diff(
-            'pretty',
+        $this->assertEquals(\Craftworks\GenDiff\compare(
             'tests/fixtures/before2.yml',
             'tests/fixtures/after2.yml'
         ), $expected2);
